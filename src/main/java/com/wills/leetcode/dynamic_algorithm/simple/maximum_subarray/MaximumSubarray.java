5d0481e11de51fc4edba717f04bd58fe4d4e6724
王帅 <loveing490@qq.com>
2021-04-08 10:34:57 +0800

    【新增】2021-04-08 新增动态规划主题题目 - 最大子序和

--- src/main/java/com/wills/leetcode/dynamic_algorithm/simple/maximum_subarray/MaximumSubarray.java
+++ src/main/java/com/wills/leetcode/dynamic_algorithm/simple/maximum_subarray/MaximumSubarray.java
@@ -1,4 +1,4 @@
-package com.wills.leetcode.maximum_subarray;
+package com.wills.leetcode.dynamic_algorithm.simple.maximum_subarray;
 
 /**
  * @author 王帅
@@ -14,14 +14,6 @@ public class MaximumSubarray {
      * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
      * 输出：6
      * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
-     * <p>
-     * 示例 2：
-     * 输入：nums = [1]
-     * 输出：1
-     * <p>
-     * 示例 3：
-     * 输入：nums = [0]
-     * 输出：0
      * <p>
      * ****进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。***
      */
@@ -48,8 +40,8 @@ public class MaximumSubarray {
      */
     public static int maxSubArray(int[] nums) {
         if (nums.length == 1) return nums[0];
-        int res = nums[0], cur_num = 0;
-        for (int i = 1; i < nums.length; i++) {
+        int res = 0, cur_num = 0;
+        for (int i = 0; i < nums.length; i++) {
             cur_num = Math.max(nums[i], cur_num + nums[i]);
 
             res = Math.max(cur_num, res);
