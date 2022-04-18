c714cd286180ffa39d2528729d5266b85a62263a
王帅 <tinx.wills@icloud.com>
2022-04-18 13:06:16 +0800

    【新增】2022-04-18 力扣LeetCode 数组排序整理

--- src/main/java/com/wills/leetcode/array/sort_array/source/impl/QuickSort.java
+++ src/main/java/com/wills/leetcode/array/sort_array/source/impl/QuickSort.java
@@ -1,7 +1,9 @@
 package com.wills.leetcode.array.sort_array.source.impl;
 
 import com.wills.leetcode.array.sort_array.source.Sort;
+import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -13,72 +15,55 @@ import java.util.Random;
  */
 public class QuickSort implements Sort {
 
-    // 列表大小等于或小于该大小，将优先于 mergeSort 使用插入排序
-    private static final int INSERTION_SORT_THRESHOLD = 7;
+    @Test
+    public void test() {
+        int[] source = {4, 6, 78, 2, 1, 6, 7, 7, 8, 342, 1};
+        quickSort(source, 0, source.length - 1);
+        System.out.println("=======================================");
+        System.out.println(Arrays.toString(source));
+        System.out.println("=======================================");
+    }
+
     // 快速排序
     @Override
     public int[] sort(int[] nums) {
-        int len = nums.length;
-        quickSort(nums, 0, len - 1);
-        return nums;
+        return quickSort(nums, 0, nums.length - 1);
     }
 
-    private void quickSort(int[] nums, int left, int right) {
-        // 小区间使用插入排序
-        if (right - left <= INSERTION_SORT_THRESHOLD) {
-            insertionSort(nums, left, right);
-            return;
+    public int[] quickSort(int[] array, int left, int right) {
+        if (left < right) {
+            // 获取划分子数组的位置
+            int position = partition(array, left, right);
+            // 左子数组递归调用
+            quickSort(array, left, position - 1);
+            // 右子数组递归调用
+            quickSort(array, position + 1, right);
         }
-
-        int pIndex = partition(nums, left, right);
-        quickSort(nums, left, pIndex - 1);
-        quickSort(nums, pIndex + 1, right);
+        return array;
     }
 
-    private static final Random RANDOM = new Random();
-    private int partition(int[] nums, int left, int right) {
-        int randomIndex = RANDOM.nextInt(right - left + 1) + left;
-        swap(nums, left, randomIndex);
-
-        // 基准值
-        int pivot = nums[left];
-        int lt = left;
-        // 循环不变量：
-        // all in [left + 1, lt] < pivot
-        // all in [lt + 1, i) >= pivot
-        for (int i = left + 1; i <= right; i++) {
-            if (nums[i] < pivot) {
-                lt++;
-                swap(nums, i, lt);
+    public int partition(int[] array, int left, int right) {
+        // 取最后一个元素作为中心元素
+        int pivot = array[right];
+        // 定义指向比中心元素大的指针，首先指向第一个元素
+        int pointer = left;
+        // 遍历数组中的所有元素，将比中心元素大的放在右边，比中心元素小的放在左边
+        for (int i = left; i < right; i++) {
+            if (array[i] <= pivot) {
+                // 将比中心元素小的元素和指针指向的元素交换位置
+                // 如果第一个元素比中心元素小，这里就是自己和自己交换位置，指针和索引都向下一位移动
+                // 如果元素比中心元素大，索引向下移动，指针指向这个较大的元素，直到找到比中心元素小的元素，并交换位置，指针向下移动
+                int temp = array[i];
+                array[i] = array[pointer];
+                array[pointer] = temp;
+                pointer++;
             }
+            System.out.println(Arrays.toString(array));
         }
-        swap(nums, left, lt);
-        return lt;
-    }
-
-    /**
-     * 对数组 arr 的子区间 [left, right] 使用插入排序
-     *
-     * @param arr   给定数组
-     * @param left  左边界，能取到
-     * @param right 右边界，能取到
-     */
-    private void insertionSort(int[] arr, int left, int right) {
-        for (int i = left + 1; i <= right; i++) {
-            int temp = arr[i];
-            int j = i;
-            while (j > left && arr[j - 1] > temp) {
-                arr[j] = arr[j - 1];
-                j--;
-            }
-            arr[j] = temp;
-        }
-    }
-
-    // 就是简单的交换值
-    private void swap(int[] nums, int index1, int index2) {
-        int temp = nums[index1];
-        nums[index1] = nums[index2];
-        nums[index2] = temp;
+        // 将中心元素和指针指向的元素交换位置
+        int temp = array[pointer];
+        array[pointer] = array[right];
+        array[right] = temp;
+        return pointer;
     }
 }
