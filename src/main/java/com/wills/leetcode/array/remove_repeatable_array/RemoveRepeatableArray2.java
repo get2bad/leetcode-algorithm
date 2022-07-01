86050f163f95de5cf717395b4fc72c422da6d919
王帅 <tinx.wills@icloud.com>
2022-07-01 14:45:01 +0800

    【新增】2022-01-01 力扣LeetCode 删除重复元素

---
+++ src/main/java/com/wills/leetcode/array/remove_repeatable_array/RemoveRepeatableArray2.java
@@ -0,0 +1,38 @@
+package com.wills.leetcode.array.remove_repeatable_array;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @ClassName RemoveRepeatableArray
+ * @Date 2022/7/1 11:35
+ * @Author 王帅
+ * @Version 1.0
+ * @Description
+ */
+public class RemoveRepeatableArray2 {
+
+    //    private static int[] source = {1, 1, 1, 2, 2, 3};
+    private static int[] source = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+
+    @Test
+    public void test() {
+        System.out.println(removeDuplicates(source));
+        System.out.println(Arrays.toString(source));
+    }
+
+    /**
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，
+     * 使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+     */
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int n : nums)
+            if (i < 2 || n > nums[i - 2])
+                nums[i++] = n;
+        return i;
+    }
+}
