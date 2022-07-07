1e61478776a8c3d31fb9f6b66e38e73cea025230
王帅 <tinx.wills@icloud.com>
2022-07-07 12:03:02 +0800

    【新增】2022-07-07 力扣LeetCode 只出现一次的数字

--- src/main/java/com/wills/leetcode/array/gas_station/GasStation.java
+++ src/main/java/com/wills/leetcode/array/gas_station/GasStation.java
@@ -16,19 +16,36 @@ public class GasStation {
 
     @Test
     public void test() {
-        System.out.println(canCompleteCircuit(gas,cost));
+        System.out.println(canCompleteCircuit1(gas, cost));
+    }
+
+    public int canCompleteCircuit1(int[] gas, int[] cost) {
+        int len = gas.length, cur = 0;
+        while (cur < len) {
+            int sumOfGas = 0, sumOfCost = 0, cnt = 0;
+            while (cnt < len) {
+                int j = (cur + cnt) % len;
+                sumOfGas += gas[j];
+                sumOfCost += cost[j];
+                if (sumOfCost > sumOfGas) break;
+                cnt++;
+            }
+            if (cnt == len) return cur;
+            else cur += cnt + 1;
+        }
+        return -1;
     }
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int rest = 0, run = 0, start = 0;
-        for (int i = 0; i < gas.length; ++i){
+        for (int i = 0; i < gas.length; ++i) {
             run += (gas[i] - cost[i]);
             rest += (gas[i] - cost[i]);
-            if (run < 0){
+            if (run < 0) {
                 start = i + 1;
                 run = 0;
             }
         }
-        return rest < 0 ? -1: start;
+        return rest < 0 ? -1 : start;
     }
 }
