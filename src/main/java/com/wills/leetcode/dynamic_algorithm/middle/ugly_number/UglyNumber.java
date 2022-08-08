fff3fc36a0e1d527d332e8c9b2b333c447f031bf
王帅 <tinx.wills@icloud.com>
2022-08-08 17:04:47 +0800

    【新增】2022-08-08 LeetCode 动态规划-丑数 1 2

---
+++ src/main/java/com/wills/leetcode/dynamic_algorithm/middle/ugly_number/UglyNumber.java
@@ -0,0 +1,28 @@
+package com.wills.leetcode.dynamic_algorithm.middle.ugly_number;
+
+import org.junit.Test;
+
+/**
+ * @ClassName UglyNumber
+ * @Date 2022/8/8 11:21
+ * @Author 王帅
+ * @Version 1.0
+ * @Description
+ */
+public class UglyNumber {
+
+    private static final int[] primes = {2,7,13,19};
+    private static final int n = 12;
+
+    @Test
+    public void test(){
+        System.out.println(nthSuperUglyNumber(n, primes));
+    }
+
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int res= 0;
+
+
+        return res;
+    }
+}
